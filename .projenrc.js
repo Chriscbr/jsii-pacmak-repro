33a28b5607e391fe3ab413c37feeab041fc1f4fe
Christopher Rybicki <rybickic@amazon.com>
2021-12-20 12:19:06 -0500

    add shx bundledDep and java publishing target

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,21 @@
 const { cdk } = require('projen');
+
 const project = new cdk.JsiiProject({
+  // defaults
   author: 'Christopher Rybicki',
   authorAddress: 'rybickic@amazon.com',
   defaultReleaseBranch: 'main',
   name: 'jsii-pacmak-repro',
-  repositoryUrl: 'https://github.com/rybickic/jsii-pacmak-repro.git',
+  repositoryUrl: 'https://github.com/Chriscbr/jsii-pacmak-repro.git',
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+  // added
+  bundledDeps: ['shx'],
+  publishToMaven: {
+    javaPackage: 'org.acme',
+    mavenGroupId: 'org.acme',
+    mavenArtifactId: 'example',
+    mavenEndpoint: 'https://s01.oss.sonatype.org',
+  },
 });
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="classes"></a>
+
+### Hello <a name="jsii-pacmak-repro.Hello" id="jsiipacmakreprohello"></a>
+
+#### Initializers <a name="jsii-pacmak-repro.Hello.Initializer" id="jsiipacmakreprohelloinitializer"></a>
+
+```typescript
+import { Hello } from 'jsii-pacmak-repro'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| [`sayHello`](#jsiipacmakreprohellosayhello) | *No description.* |
+
+---
+
+##### `sayHello` <a name="jsii-pacmak-repro.Hello.sayHello" id="jsiipacmakreprohellosayhello"></a>
+
+```typescript
+public sayHello()
+```
+
+
+
+
+
